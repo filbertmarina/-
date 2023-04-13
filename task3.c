fdcf0a94dc30d814249efc872838be6f70c4ac70
filbertmarina <112921911+filbertmarina@users.noreply.github.com>
2023-04-13 22:15:39 +0700

    Update task3.c

--- task3.c
+++ task3.c
@@ -7,14 +7,20 @@
 #include <cublas_v2.h>
 
 
-
+///////////////////////////////////////////////////
+// Фунция main
+///////////////////////////////////////////////////
 int main(int argc, char**argv) {
+
+//Установка параметров 
 int SIZE;
 double err_max;
 int iter_max;
 SIZE = atoi(argv[2]);
 err_max=atof(argv[1]);
 iter_max = atoi(argv[3]);
+	
+	
 	cublasHandle_t handle;
 	cublasStatus_t stat;
 
@@ -23,11 +29,14 @@ iter_max = atoi(argv[3]);
 		printf("CUBLAS initialization failed\n");
 		return EXIT_FAILURE;
 	}
-
+//Запуск clock();
 	clock_t start;
 	start = clock();
 
-
+	
+	//////////////////////////////////
+	//Заполнение массивов mas, mas_old, выделение памяти на cpu
+	//////////////////////////////////
 	double* mas = (double*)calloc(SIZE * SIZE, sizeof(double));
 	double* mas_old = (double*)calloc(SIZE * SIZE, sizeof(double));
 
@@ -35,8 +44,6 @@ iter_max = atoi(argv[3]);
 	mas[SIZE - 1] = 20;
 	mas[(SIZE) * (SIZE - 1)] = 20;
 	mas[(SIZE) * (SIZE)-1] = 30;
-
-
 
 	for (int i = 1; i < SIZE - 1; i++)
 		mas[i] = mas[i - 1] + (mas[SIZE - 1] - mas[0]) / SIZE;
@@ -47,16 +54,20 @@ iter_max = atoi(argv[3]);
 		mas[(SIZE) * (i)+(SIZE - 1)] = mas[(SIZE) * (i - 1) + (SIZE - 1)] + (mas[(SIZE) * (SIZE)-1] - mas[SIZE - 1]) / SIZE;
 	}
 
-
+	for (int i = 0; i < SIZE * SIZE; i++){
+	mas_old[i] = mas[i];
+}
+	
+	
+	
 	int iter = 0;
 	double err = 1.0;
 
-
-	for (int i = 0; i < SIZE * SIZE; i++){
-	mas_old[i] = mas[i];
 	
-}
-
+	///////////////////////////////////////////////////////////////////////
+	//Основной цикл программы: проверка максимальных значений, нахождение вектора ошибок, нахождение максимальной ошибки,
+	// перезапись mas в mas_old
+	///////////////////////////////////////////////////////////////////////
 #pragma acc enter data copyin(mas[0:SIZE*SIZE], mas_old[0:SIZE*SIZE]) 
 	{
 			double a = -1;
@@ -85,7 +96,7 @@ iter_max = atoi(argv[3]);
 #pragma acc host_data use_device(mas,mas_old)
 				{
 
-	
+	//(на gpu)нахождение вектора разницы массивов mas и mas_old, запись в mas_old
 				stat = cublasDaxpy(handle, SIZE * SIZE, &a, (const double*)mas, 1, mas_old, 1);
 					if (stat != CUBLAS_STATUS_SUCCESS) {
 						printf("CUBLAS initialization failed1\n");
@@ -93,7 +104,7 @@ iter_max = atoi(argv[3]);
 					}
 
 
-
+	//(на gpu)нахождение индекса максимального значения в mas_old
 					stat =cublasIdamax(handle, SIZE*SIZE, mas_old, 1, &index);
 					if (stat != CUBLAS_STATUS_SUCCESS) {
 						printf("CUBLAS initialization failed2\n");
@@ -104,16 +115,17 @@ iter_max = atoi(argv[3]);
 
 				}
 
-
+//обновляем найденную ошибку на cpu
 #pragma acc update host(mas_old[index - 1]) 
 				err = fabs(mas_old[index - 1]);
 
 
-
+//перезапись массивов
 #pragma acc host_data use_device(mas_old, mas)
 				stat = cublasDcopy(handle, SIZE * SIZE, mas, 1, mas_old, 1);
 			}
 
+			
 			double* m = mas;
 			mas = mas_old;
 			mas_old = m;
@@ -121,7 +133,7 @@ iter_max = atoi(argv[3]);
 
 iter += 1;
 
-
+//вывод времени работы и ошибки 
 			if (iter % 100 == 0 && iter != 1) {
 				double t = (double)(clock() - start) / CLOCKS_PER_SEC;
 				printf(" time: %lf\n", t);
@@ -130,14 +142,14 @@ iter += 1;
 			}
 		}
 	}
+	
 	cublasDestroy(handle);
 
-	//for (int i = 0; i < SIZE * SIZE; i++)
+//using free to free cpu memory
 	free(mas_old);
-
-	//for (int i = 0; i < SIZE*SIZE; i++)
 	free(mas);
 
+//вывод общего времени работы программы, функции clock()
 	double t = (double)(clock() - start) / CLOCKS_PER_SEC;
 	printf(" time: %lf\n", t);
 	return EXIT_SUCCESS;
